bf0bab5e9159a109a498707ca7c4515d9f61e4e5
DejViczu12 <126686272+169724@users.noreply.github.com>
2023-03-15 14:34:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    
+    for(int cels = 0; cels <301; cels++ ){
+       if(cels % 20==0){
+        System.out.println("Stopnie w Celsjuszach: " +cels );
+        System.out.println("Farenheity = " +9/5 * (cels + 32));
+}
+}
+}
+}
